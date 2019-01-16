5dd690163d26bdeb61b36cb29fc13f9a0d71a98f
Kevin Singh <kevinsingh.developer@gmail.com>
2019-01-16 13:07:35 -0500

    Create BasicObjectType.cs

---
+++ BasicObjectType.cs
@@ -0,0 +1,7 @@
+// object data types
+// this is an reference type. Indexes address of a memory area which stores the actual value
+// Object takes any type
+object i = 5;
+
+
+Console.WriteLine(i);
